43bfdd432d647d7e44cdff707c2c5080a393e893
pxffzz <ansaranwari1234@gmail.com>
2025-11-08 18:53:14 -0500

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,41 +1,83 @@
+-- Load Rayfield
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
+-- Create Window
 local Window = Rayfield:CreateWindow({
-   Name = "COPD OP GUI 🔥",
-   Icon = flame, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
-   LoadingTitle = "C0PD OP GUI 🔥",
-   LoadingSubtitle = "Made By MZT3R",
-   ShowText = "HIDE or SHOW", -- for mobile users to unhide rayfield, change if you'd like
-   Theme = "Amethyst", -- Check https://docs.sirius.menu/rayfield/configuration/themes
+    Name = "COPD OP GUI 🔥",
+    Icon = "flame", -- Must be a string if using Lucide icons
+    LoadingTitle = "C0PD OP GUI 🔥",
+    LoadingSubtitle = "Made By MZT3R",
+    ShowText = "HIDE or SHOW",
+    Theme = "Amethyst",
 
-   ToggleUIKeybind = "X", -- The keybind to toggle the UI visibility (string like "K" or Enum.KeyCode)
+    ToggleUIKeybind = "X", -- Key to toggle GUI
 
-   DisableRayfieldPrompts = false,
-   DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
+    DisableRayfieldPrompts = false,
+    DisableBuildWarnings = false,
 
-   ConfigurationSaving = {
-      Enabled = true,
-      FolderName = C0PD OP GUI Folder, -- Create a custom folder for your hub/game
-      FileName = "C0PD OP GUI 🔥"
-   },
+    ConfigurationSaving = {
+        Enabled = true,
+        FolderName = "C0PD_OP_GUI_Folder", -- must be a string
+        FileName = "C0PD OP GUI 🔥"
+    },
 
-   Discord = {
-      Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
-      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
-      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
-   },
+    Discord = {
+        Enabled = false,
+        Invite = "",
+        RememberJoins = true
+    },
 
-   KeySystem = true, -- Set this to true to use our key system
-   KeySettings = {
-      Title = "Key System",
-      Subtitle = "Key System For C0PD GUI OP 🔥",
-      Note = "Get Key In ScriptBlox Description! 💫", -- Use this to tell the user how to get a key
-      FileName = "KEY", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-      GrabKeyFromSite = true, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"dedend"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("end","ded")
-   }
+    KeySystem = true, -- Enable key system
+    KeySettings = {
+        Title = "Key System",
+        Subtitle = "Key System For C0PD GUI OP 🔥",
+        Note = "Get Key In ScriptBlox Description! 💫",
+        FileName = "KEY",
+        SaveKey = true,
+        GrabKeyFromSite = false, -- Only true if Key is a RAW URL
+        Key = {"dedend"} -- List of accepted keys
+    }
 })
 
-local Tab = Window:CreateTab("HOME", "house")
-local Section = Tab:CreateSection("HOME")
+-- Create Tabs and Sections
+local HomeTab = Window:CreateTab("HOME", "house")
+local HomeSection = HomeTab:CreateSection("HOME")
+
+-- Example Button
+HomeSection:CreateButton({
+    Name = "Example Button",
+    Callback = function()
+        Rayfield:Notify({
+            Title = "Button Pressed",
+            Content = "You pressed the example button!",
+            Duration = 3
+        })
+    end
+})
+
+-- Example Toggle
+HomeSection:CreateToggle({
+    Name = "Example Toggle",
+    CurrentValue = false,
+    Flag = "ExampleToggle",
+    Callback = function(value)
+        if value then
+            print("Toggle ON")
+        else
+            print("Toggle OFF")
+        end
+    end
+})
+
+-- Example Slider
+HomeSection:CreateSlider({
+    Name = "Example Slider",
+    Range = {0, 100},
+    Increment = 1,
+    Suffix = "%",
+    CurrentValue = 50,
+    Flag = "ExampleSlider",
+    Callback = function(value)
+        print("Slider set to:", value)
+    end
+})
